5ae98dab345555e28db20f5560363ff47e9b6076
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-03-28 23:17:05 +0530

    added greatest_of_three function in  basic_functions
    1. used greatest_of_two to find greatest_of_three
    2. fixed bug of not getting floating values in average

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -27,7 +27,19 @@ int cube(int number)
 float average(int number_one, int number_two, int number_three);
 float average(int number_one, int number_two, int number_three)
 {
-  return (number_one + number_two + number_three) / 3;
+  return (number_one + number_two + number_three) / 3.00;
+}
+
+int greatest_of_two(int number_one, int number_two);
+int greatest_of_two(int number_one, int number_two)
+{
+  return number_one > number_two ? number_one : number_two;
+}
+
+int greatest_of_three(int number_one, int number_two, int number_three);
+int greatest_of_three(int number_one, int number_two, int number_three)
+{
+  return greatest_of_two(greatest_of_two(number_one, number_two), number_three);
 }
 
 int main(void)
@@ -39,6 +51,9 @@ int main(void)
   int number_one_for_average;
   int number_two_for_average;
   int number_three_for_average;
+  int number_one_for_greatest;
+  int number_two_for_greatest;
+  int number_three_for_greatest;
 
   printf("\nEnter a number for is_even \n");
   scanf("%d", &number_for_even);
@@ -57,9 +72,15 @@ int main(void)
   scanf("%d", &number_two_for_average);
   scanf("%d", &number_three_for_average);
 
+  printf("\nEnter 3 numbers two find greatest of those numbers \n");
+  scanf("%d", &number_one_for_greatest);
+  scanf("%d", &number_two_for_greatest);
+  scanf("%d", &number_three_for_greatest);
+
   printf("\nThe number %d is %s \n", number_for_even, is_even(number_for_even) ? "even" : "not even");
   printf("The number %d is %s \n", number_for_odd, is_odd(number_for_odd) ? "odd" : "not odd");
   printf("Square of %d is %d \n", number_for_square, square(number_for_square));
   printf("Cube of %d is %d \n", number_for_cube, cube(number_for_cube));
   printf("Average of %d %d %d is %f \n", number_one_for_average, number_two_for_average, number_three_for_average, average(number_one_for_average, number_two_for_average, number_three_for_average));
+  printf("Greatest of %d %d %d is %d \n", number_one_for_greatest, number_two_for_greatest, number_three_for_greatest, greatest_of_three(number_one_for_greatest, number_two_for_greatest, number_three_for_greatest));
 }
