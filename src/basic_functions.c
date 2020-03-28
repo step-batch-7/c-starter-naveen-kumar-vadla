29103bf6ecc05f2a118734700fb1e8ae33a6d2f0
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-03-28 23:00:59 +0530

    added average function in  basic_functions.c

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -24,12 +24,21 @@ int cube(int number)
   return number * square(number);
 }
 
+float average(int number_one, int number_two, int number_three);
+float average(int number_one, int number_two, int number_three)
+{
+  return (number_one + number_two + number_three) / 3;
+}
+
 int main(void)
 {
   int number_for_even;
   int number_for_odd;
   int number_for_square;
   int number_for_cube;
+  int number_one_for_average;
+  int number_two_for_average;
+  int number_three_for_average;
 
   printf("\nEnter a number for is_even \n");
   scanf("%d", &number_for_even);
@@ -43,8 +52,14 @@ int main(void)
   printf("\nEnter a number for cube \n");
   scanf("%d", &number_for_cube);
 
+  printf("\nEnter 3 numbers two calculate average \n");
+  scanf("%d", &number_one_for_average);
+  scanf("%d", &number_two_for_average);
+  scanf("%d", &number_three_for_average);
+
   printf("\nThe number %d is %s \n", number_for_even, is_even(number_for_even) ? "even" : "not even");
   printf("The number %d is %s \n", number_for_odd, is_odd(number_for_odd) ? "odd" : "not odd");
   printf("Square of %d is %d \n", number_for_square, square(number_for_square));
   printf("Cube of %d is %d \n", number_for_cube, cube(number_for_cube));
+  printf("Average of %d %d %d is %f \n", number_one_for_average, number_two_for_average, number_three_for_average, average(number_one_for_average, number_two_for_average, number_three_for_average));
 }
