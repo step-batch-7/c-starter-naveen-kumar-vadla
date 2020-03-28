142284290fbaeac56eeb8ed0ab3a239a07bdd3ee
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-03-28 22:10:48 +0530

    modified script to run c programs

--- runC.sh
+++ runC.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
+# Script to compile and execute a c program in one step.
 
 mkdir -p outputs
 
+# Get file name 
 filename=$1
 
-outputFile=`echo ${filename} | sed 's/.\///' | sed 's/.c$//'`
+# Get outputfile name without the .c extension
+output_filename=`echo $filename | sed 's/.*\/\(.*\)\.c/\1/'`
 
-gcc -o outputs/${outputFile} ./${filename} && outputs/${outputFile}
+# Compile the program with -o option to specify the name of the binary
+gcc -o outputs/$output_filename.out $filename
 
+# If there were no compilation errors, run the program
+if [ $? -eq 0 ]; then
+        outputs/$output_filename.out
+fi
