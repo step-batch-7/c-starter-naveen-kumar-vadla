406a27cf3d6952dc3a1e5a0a11526c5c7d849bad
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-03-28 22:45:30 +0530

    added cube function in  basic_functions.c

--- src/basic_functions.c
+++ src/basic_functions.c
@@ -18,11 +18,18 @@ int square(int number)
   return number * number;
 }
 
+int cube(int number);
+int cube(int number)
+{
+  return number * square(number);
+}
+
 int main(void)
 {
   int number_for_even;
   int number_for_odd;
   int number_for_square;
+  int number_for_cube;
 
   printf("\nEnter a number for is_even \n");
   scanf("%d", &number_for_even);
@@ -33,7 +40,11 @@ int main(void)
   printf("\nEnter a number for square \n");
   scanf("%d", &number_for_square);
 
+  printf("\nEnter a number for cube \n");
+  scanf("%d", &number_for_cube);
+
   printf("\nThe number %d is %s \n", number_for_even, is_even(number_for_even) ? "even" : "not even");
   printf("The number %d is %s \n", number_for_odd, is_odd(number_for_odd) ? "odd" : "not odd");
   printf("Square of %d is %d \n", number_for_square, square(number_for_square));
+  printf("Cube of %d is %d \n", number_for_cube, cube(number_for_cube));
 }
