57e5d1045584ceedcf89c8c230c15ebf4787cdcf
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-03-29 11:39:26 +0530

    added fibonacci_series function in loops.c

--- src/loops.c
+++ src/loops.c
@@ -11,6 +11,22 @@ unsigned int factorial(int number)
   return fact;
 }
 
+int fibonacci_series(int limit);
+int fibonacci_series(int limit)
+{
+  unsigned int first_term = 0;
+  unsigned int second_term = 1;
+  for (int index = 1; index <= limit; index++)
+  {
+    printf("%u ", first_term);
+    int temp = second_term;
+    second_term += first_term;
+    first_term = temp;
+  }
+  printf("\n");
+  return 0;
+}
+
 int main(void)
 {
   printf("-------------------- Factorial --------------------");
@@ -18,4 +34,11 @@ int main(void)
   printf("\nEnter the number you want to calculate factorial\n");
   scanf("%d", &number_for_factorial);
   printf("Factorial of %d is %u\n", number_for_factorial, factorial(number_for_factorial));
+
+  printf("-------------------- Fibonacci Series --------------------");
+  int number_for_fibonacci;
+  printf("\nEnter the limit for fibonacci series\n");
+  scanf("%d", &number_for_fibonacci);
+  printf("First %d Fibonacci numbers\n", number_for_fibonacci);
+  fibonacci_series(number_for_fibonacci);
 }
