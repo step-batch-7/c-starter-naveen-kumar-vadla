bca81d97c76991c2540b74111c2f439f2a85fe83
Vadla Naveen Kumar <58026402+naveenkumarvadla@users.noreply.github.com>
2020-04-07 10:08:43 +0530

    modified script to run c program

--- runC.sh
+++ runC.sh
@@ -3,16 +3,19 @@
 
 mkdir -p outputs
 
-# Get file name 
+# Get file name
 filename=$1
 
 # Get outputfile name without the .c extension
 output_filename=`echo $filename | sed 's/.*\/\(.*\)\.c/\1/'`
+
+#remove old output_file
+rm -f outputs/$output_filename.out
 
 # Compile the program with -o option and specify the name of the binary file
 gcc -o outputs/$output_filename.out $filename
 
 # If there were no compilation errors, run the program
 if [ $? -eq 0 ]; then
-        outputs/$output_filename.out
+    outputs/$output_filename.out
 fi
